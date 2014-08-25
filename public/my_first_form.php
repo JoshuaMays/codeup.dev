2bb5a6e243da6755d05713e9032a48860f37bb7a
haydogsup <joshua.r.mays@gmail.com>
2014-08-25 11:46:28 -0500

    added compose email form

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -10,19 +10,39 @@
 	<title>My First Form</title>
 </head>
 <body>
-	<h1>My First HTML Form</h1>
+	<h2>Login</h2>
 	<form method="POST" action="/my_first_form.php">
 	    <p>
 	        <label for="username">Username</label>
-	        <input id="username" name="username"type="text" placeholder="whatcho name?">
+	        <input id="username" name="username" type="text" placeholder="whatcho name?"/>
 	    </p>
 	    <p>
 	        <label for="password">Password</label>
-	        <input id="password" name="password" type="password" placeholder="gimme dat password!">
+	        <input id="password" name="password" type="password" placeholder="gimme dat password!"/>
 	    </p>
-	    <p>
-	        <input type="submit">
-	    </p>
+	    <input type="submit">
+
+	</form>
+
+	<h2>Compose an Email</h2>
+	<form method="POST" action="/my_first_form.php">
+		<p>
+			<label for="to">To: </label>
+			<input type="email" id="to" name="to" />
+		</p>
+		<p>
+			<label for="from">From: </label>
+			<input type="email" id="from" name="from" />
+		</p>
+		<p>
+			<label for="subject">Subject: </label>
+			<input type="text" id="subject" name="subject" />
+		</p>
+		<p>
+			<label for="body">Body: </label>
+			<textarea id="body" name="body"></textarea>
+		</p>
+		<input type="submit" />
 	</form>
 </body>
 </html>
