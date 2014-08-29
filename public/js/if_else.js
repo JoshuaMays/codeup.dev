b4080b82c7f7a213ffa022051891709b3ffc080f
haydogsup <joshua.r.mays@gmail.com>
2014-08-29 11:49:53 -0500

    removed toUpperCase

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -1,11 +1,3 @@
-// var test = false;
-
-// if(test) {
-//     console.log('yay we got true!');
-// } else {
-//     console.log('ugh, it\'s false');
-// }
-
 // ignore these lines for now
 // just know that the variable 'color' will end up with a random value from the colors array
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
@@ -17,17 +9,17 @@ var favorite = 'violet'; // todo, change this to your favorite color from the li
 // todo: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
 if (color == 'red') {
-    console.log(color.toUpperCase() + ' is the color of a bloody lip.');
+    console.log(color + ' is the color of a bloody lip.');
 } else if (color == 'orange') {
-    console.log(color.toUpperCase() + ' is the color of a well-groomed Orangutan.');
+    console.log(color + ' is the color of a well-groomed Orangutan.');
 } else if (color == 'yellow') {
-    console.log(color.toUpperCase() + ' is the color of that really creepy guy from Sin City.');
+    console.log(color + ' is the color of that really creepy guy from Sin City.');
 } else if (color == 'green') {
-    console.log(color.toUpperCase() + ' is the color of a fruit that vitamin-c deficient sailors should really try to eat more often.');
+    console.log(color + ' is the color of a fruit that vitamin-c deficient sailors should really try to eat more often.');
 } else if (color == 'blue') {
-    console.log(color.toUpperCase() + ' is the color of the face of the man who didn\'t chew his food before swallowing.');
+    console.log(color + ' is the color of the face of the man who didn\'t chew his food before swallowing.');
 } else {
-    console.log(color.toUpperCase() + ': I do not know anything by that color.');
+    console.log(color + ': I do not know anything by that color.');
 }
 
 // todo: Have a final else that will catch indigo and violet.
@@ -36,4 +28,4 @@ if (color == 'red') {
 // todo: Using the ternary operator, conditionally log a statement that
 //       says whether the random color matches your favorite color.
 
-var match = (color == favorite) ? console.log('YEAH, WE GOT A MATCH!') : console.log('No match. Like a better color.');
+var match = (color == favorite) ? console.log('OH YEAH, MATCH!') : console.log('No match. Like a better color.');
