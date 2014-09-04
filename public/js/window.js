e7eae5712931e9377229aee471d82933249aec64
haydogsup <joshua.r.mays@gmail.com>
2014-09-04 00:39:35 -0500

    moved function vars to top of function.

--- public/js/window.js
+++ public/js/window.js
@@ -1,23 +1,18 @@
+// Initialize timeoutId as global var
 var timeoutId;
 function save() {
             alert('Saved!');
         }
 
 function autosave() {
-
-// todo:
-// Use setTimeout and clearTimeout within this function
-// so that the save() function is called 5 seconds after
-// the last key up event. If a new key up event occurs,
-// you need to cancel the existing timer and set a new one.
+    var timer = 5000;
+    
+    // When multiple autosave() calls are made, clear the previous timeout
     clearTimeout(timeoutId);
-    timer = 5000;
+    // Set a 5 second timeout function that calls save()
     timeoutId = setTimeout(save, timer);
 }
-// don't modify the line below
-// this causes the autosave function to be called whenever
-// a key up event occurs in the textarea
-// we will learn about events in the DOM lessons
+
 var textarea = document.getElementById('important');
 textarea.addEventListener('keyup', autosave, false);
 
