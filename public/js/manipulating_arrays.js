aaeaad0d3b8f047ea750b2176771b28d942f14fe
haydogsup <joshua.r.mays@gmail.com>
2014-09-04 00:44:01 -0500

    recommenting

--- public/js/manipulating_arrays.js
+++ public/js/manipulating_arrays.js
@@ -1,5 +1,4 @@
-// todo:
-// Create an array holding the names of the planets of our solar system (in order, starting closest to the sun).
+// Create an array holding the names of the planets of our solar system
 var planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
 
 // function for logging the planets array
@@ -13,48 +12,48 @@ function logPlanets() {
 }
 
 console.log('Adding "The Sun" to the beginning of the planets array.');
-// todo: Do what the log statement above says you will do.
+// Add The Sun to the beginning of planets array.
 planets.unshift('The Sun');
 logPlanets();
 
 console.log('Adding "Pluto" to the end of the planets array.');
-// todo: Do what the log statement above says you will do.
+// Add Pluto to the end of the planets array.
 planets.push('Pluto');
 logPlanets();
 
 console.log('Removing "The Sun" from the beginning of the planets array.');
-// todo: Do what the log statement above says you will do.
+// Shift off The Sun from the beginning of the array.
 planets.shift();
 logPlanets();
 
 console.log('Removing "Pluto" from the end of the planets array.');
-// todo: Do what the log statement above says you will do.
+// Pop off Pluto from the end of the planets array.
 planets.pop();
 logPlanets();
 
 console.log('Finding and logging the index of "Earth" in the planets array.');
-// todo: Do what the log statement above says you will do.
+// Capture the index of "Earth".
 var earthIndex = planets.indexOf('Earth');
 console.log('Earth is at index: ' + earthIndex);
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 
 
 console.log('Using splice to remove the planet after Earth.');
-// todo: Do what the log statement above says you will do.
+// Splice out the planet after earthIndex. Capture removed planet to var.
 var splicedPlanet = planets.splice(earthIndex + 1,1)
 logPlanets();
 
 console.log('Using splice to add back the planet after Earth.');
-// todo: Do what the log statement above says you will do.
+// Splice the planet back in again.
 planets.splice(earthIndex + 1, 0, splicedPlanet[0]);
 logPlanets();
 
 console.log('Reversing the order of the planets array.');
-// todo: Do what the log statement above says you will do.
+// Reverse the array elements.
 planets.reverse();
 logPlanets();
 
 console.log('Sorting the planets array.');
-// todo: Do what the log statement above says you will do.
+// Sort the array alphabetically.
 planets.sort();
 logPlanets();
