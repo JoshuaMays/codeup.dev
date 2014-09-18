5ed916c861a5d385af6c89648957adefbed15a51
haydogsup <joshua.r.mays@gmail.com>
2014-09-18 17:56:50 -0500

    hide prev/next buttons when they aren't necessary

--- public/national_parks.php
+++ public/national_parks.php
@@ -3,22 +3,21 @@
 // CREATE NEW INSTANCE OF PDO OBJECT
 require '../dbconnection.php';
 
-// INITIALIZE LIMIT AND OFFSET FOR SELECT STATEMENTS
-$limit = 4;
-$offset = 0;
-
-if (!empty($_GET)) {
-    $limit = $_GET['limit'];
-    $offset = $_GET['offset'];
-}
+// INITIALIZE OFFSET FOR SELECT STATEMENTS
+$offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
 
 // FUNCTION TO RETURN ROWS OF PARK DATA FROM THE DATABASE
-function getParks($dbc, $limit, $offset) {
-    $query = "SELECT * FROM national_parks LIMIT " . $limit . " OFFSET " . $offset;
+function getParks($dbc, $offset) {
+    $query = "SELECT name, location, area_in_acres, date_established FROM national_parks LIMIT 4 OFFSET $offset";
+
     return $dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
 }
 
-$stmt = getParks($dbc, $limit, $offset);
+// CREATING ARRAY FROM SELECTED RECORD SET
+$stmt = getParks($dbc, $offset);
+
+// ASSIGNING COUNT OF RECORDS IN TABLE TO A VARIABLE FOR PAGINATION LINKS
+$count = (int) $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
 
 ?>
 
@@ -30,26 +29,35 @@ $stmt = getParks($dbc, $limit, $offset);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 </head>
 <body>
-
-<table class="table table-striped">
-    <th>Name</th>
-    <th>Location</th>
-    <th>Area (acres)</th>
-    <th>Date Established</th>
-    <? foreach ($stmt as $parkInfo): ?>
-        <tr>
-            <td><?= $parkInfo['name']; ?></td>
-            <td><?= $parkInfo['location']; ?></td>
-            <td><?= $parkInfo['area_in_acres']; ?></td>
-            <td class="estabDate"><?= $parkInfo['date_established']; ?></td>
-        </td>
-    <? endforeach; ?>
-</table>
-</div>
-<p>Page: <a href="national_parks.php?limit=4&offset=0">1</a>, 
-     <a href="national_parks.php?limit=4&offset=4">2</a>, 
-     <a href="national_parks.php?limit=4&offset=8">3</a>
-</p>
+    <div class="container">
+        <div class="row">
+            <h1>US National Parks</h1>
+            <table class="table table-striped">
+                <th>Name</th>
+                <th>Location</th>
+                <th>Area (acres)</th>
+                <th>Date Established</th>
+                <!-- LOOP THROUGH THE SELECTED RECORDS TO DISPLAY PARK INFO-->
+                <? foreach ($stmt as $parkInfo): ?>
+                    <tr>
+                        <td><?= $parkInfo['name']; ?></td>
+                        <td><?= $parkInfo['location']; ?></td>
+                        <td><?= number_format($parkInfo['area_in_acres'],2); ?></td>
+                        <td class="estabDate"><?= $parkInfo['date_established']; ?></td>
+                    </tr>
+                <? endforeach; ?>
+            </table>
+        </div>
+        <!-- HIDE PREV BUTTON WHEN AT BEGINNING OF RECORDS -->
+        <? if ($offset != 0): ?>
+            <a href="?offset=<?=$offset-4;?>"><button class="btn btn-primary">Prev</button></a>
+        <? endif; ?>
+        
+        <!-- HIDE NEXT BUTTON WHEN AT END OF RECORDS -->
+        <? if (($offset+4) < $count): ?>
+            <a href="?offset=<?=$offset+4;?>"><button class="btn btn-primary pull-right">Next</button></a>
+        <? endif; ?>
+    </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
@@ -57,6 +65,7 @@ $stmt = getParks($dbc, $limit, $offset);
 <!-- Moment JS -->
 <script src="js/moment.js"></script>
 <script>
+// FORMAT DATES HUMAN-READABLE LONG-FORM
 var dates = document.getElementsByClassName('estabDate');
 for (var i = 0; i < dates.length; i++) {
     dates[i].innerText = moment(dates[i].innerText, 'YYYY MMM DD').format('MMMM D, YYYY');
