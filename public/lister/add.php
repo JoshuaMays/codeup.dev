e43e0398cdf9e1b44c29d0c9bb3490dda839db22
haydogsup <joshua.r.mays@gmail.com>
2014-09-22 00:49:12 -0500

    added image functionality to new ad page

--- public/lister/add.php
+++ public/lister/add.php
@@ -1,34 +1,36 @@
 <?php
-require_once 'classes/ad_manager.class.php';
+require '../../adlisterconnect.php';
 require_once 'classes/ad.class.php';
 
-// ALLOW USER TO UPLOAD AN IMAGE TO THEIR AD
+// IF A FILE HAS BEEN SUCCESSFULLY UPLOADED TO THE FORM, MOVE IMAGE FILE
+// TO THE IMG FOLDER AND SAVE A WEB PATH
 if (count($_FILES) > 0 && $_FILES['fileUpload']['error'] == UPLOAD_ERR_OK) {
     // UPLOAD DIRECTORY PATH
     $uploadDir = '/vagrant/sites/codeup.dev/public/lister/img/';
     $uploadFilename = basename($_FILES['fileUpload']['name']);
+
     // UPLOADED PATH AND FILENAME
     $savedFile = $uploadDir . $uploadFilename;
-    // MOVE TMP FILE TO UPLOADS DIRECTORY
+
+    // MOVE TMP FILE TO IMAGE DIRECTORY
     move_uploaded_file($_FILES['fileUpload']['tmp_name'], $savedFile);
-    $img = $uploadFilename;
+
+    // IMAGE PATH TO BE INSERTED INTO DATABASE
+    $webPath = "img/" . $uploadFilename;
 }
 
 if(!empty($_POST)) {
-    // CREATE AD MANAGER OBJECT
-    $adManager = new AdManager();
-    $ads = $adManager->showAds();
-    $listedDate = date('Y M d');
-    
     // CREATE AND POPULATE AD OBJECT WITH FORM DATA
-    $ad = new Ad($_POST['postTitle'], $_POST['postBody'], $listedDate, $_POST['postAuthor'], $_POST['postEmail'], $_POST['postImage']);
-
-    $ads[] = $ad;
-    
-    $adManager->saveAds($ads);
+    $ad = new Ad($dbc);
+    $ad->title        = $_POST['title'];
+    $ad->body         = $_POST['body'];
+    $ad->contactName  = $_POST['contact_name'];
+    $ad->contactEmail = $_POST['contact_email'];
+    $ad->imagePath    = $_POST['image_path'];
+    $ad->save();
     
     // REDIRECT USER TO VIEW PAGE FOR AD JUST CREATED
-    header('location: view.php?id=' . (count($ads) - 1));
+    header('location: view.php?id=' . $ad->id);
     exit;
 }
 
@@ -39,7 +41,7 @@ if(!empty($_POST)) {
 <div class="container">
     <h2 class="text-center">List an Item</h2>
     <div class="row">
-        <!-- DISPLAY IMAGE UPLOAD FORM BEFORE USER CAN POST AN AD -->
+        <? // DISPLAY IMAGE UPLOAD FORM BEFORE USER CAN POST AN AD ?>
         <? if (count($_FILES) == 0): ?>
         <form method="POST" enctype="multipart/form-data" action="/lister/add.php" role="form" class="form-horizontal">
             <p class="text-center"><em>Please upload an image before starting an ad.</em></p>
@@ -52,37 +54,37 @@ if(!empty($_POST)) {
                 </div>
             </div>
         </form>
-        <? endif; ?>
-        <!-- DISPLAY MAIN FORM AFTER USER HAS UPLOADED AN IMAGE -->
+        <?  endif; ?>
+        <? // DISPLAY MAIN FORM AFTER USER HAS UPLOADED AN IMAGE ?>
         <? if (count($_FILES) > 0 && $_FILES['fileUpload']['error'] == UPLOAD_ERR_OK): ?>
-            <form method="POST" action="/lister/add.php" role="form" class="form-horizontal">
+            <form method="POST" role="form" class="form-horizontal">
                 <div class="form-group">
-                    <label for="postTitle" class="col-sm-2 control-label">Title:</label>
+                    <label for="title" class="col-sm-2 control-label">Title:</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control" name="postTitle" id="postTitle" placeholder="Title">
+                        <input type="text" class="form-control" name="title" id="title" placeholder="Title">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="postBody" class="col-sm-2 control-label">Body:</label>
+                    <label for="body" class="col-sm-2 control-label">Body:</label>
                     <div class="col-sm-9">
-                        <textarea class="form-control" name="postBody" id="postBody" rows="10" placeholder="Describe the item that you're listing here. Be creative..."></textarea>
+                        <textarea class="form-control" name="body" id="body" rows="10" placeholder="Describe the item that you're listing here. Be creative..."></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="postAuthor" class="col-sm-2 control-label">Name:</label>
+                    <label for="contact_name" class="col-sm-2 control-label">Name:</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control" name="postAuthor" id="postAuthor" placeholder="Paul Bunyan">
+                        <input type="text" class="form-control" name="contact_name" id="contact_name" placeholder="Paul Bunyan">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="postEmail" class="col-sm-2 control-label">Email:</label>
+                    <label for="contact_email" class="col-sm-2 control-label">Email:</label>
                     <div class="col-sm-9">
-                        <input type="email" class="form-control" name="postEmail" id="postEmail" placeholder="paul.bunyan@email.com">
+                        <input type="email" class="form-control" name="contact_email" id="contact_email" placeholder="paul.bunyan@email.com">
                     </div>
                 </div>
-                <!-- HIDDEN INPUT TO HOLD UPLOADED IMAGE INFORMATION -->
+                <? // HIDDEN INPUT TO POST UPLOADED IMAGE INFORMATION ?>
                 <? if (count($_FILES) > 0 && $_FILES['fileUpload']['error'] == UPLOAD_ERR_OK):?>
-                    <input type="hidden" value="<?=$img ?>" name="postImage">
+                    <input type="hidden" value="<?= $webPath; ?>" name="image_path">
                 <? endif; ?>
                 <div class="form-group">
                     <div class="col-sm-10 col-sm-offset-2">
