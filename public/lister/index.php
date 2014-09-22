9ea7043b8f08e85bf9b52d3e91d93d915fa21d8f
haydogsup <joshua.r.mays@gmail.com>
2014-09-22 00:44:04 -0500

    changed index page to display ads from database

--- public/lister/index.php
+++ public/lister/index.php
@@ -1,10 +1,11 @@
 <?php
-require_once 'classes/ad_manager.class.php';
-require_once 'classes/ad.class.php';
 
-// CREATE AD MANAGER AND ADS OBJECTS
-$adManager = new AdManager();
-$ads = $adManager->showAds();
+require '../../adlisterconnect.php';
+require_once 'classes/ad_manager.class.php';
+
+// CREATE AD MANAGER AND ADS ARRAY
+$adManager = new AdManager($dbc);
+$ads = $adManager->loadAds();
 ?>
 
 <? include 'header.php'; ?>
@@ -17,15 +18,16 @@ $ads = $adManager->showAds();
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2">
             <div class="row">
-                <!-- LOOP THROUGH ADS TO DISPLAY ALL ADS IN CSV DATASTORE -->
+                <? // LOOP THROUGH ADS TO DISPLAY ALL ADS IN DATABASE ?>
                 <? foreach($ads as $adIndex => $adContent): ?>
-                    <div class="col-sm-4 col-md-4 <?= $adIndex % 3 == 0 ? 'clear' : '' ?>">
+                    <!-- ASSIGN CLEAR CLASS TO EVERY THIRD AD, SO ROWS OF THREE ADS LINE UP CORRECTLY -->
+                    <div class="col-sm-4 col-md-4 <?= $adIndex % 3 == 0 ? 'clear' : ''; ?>">
                         <div class="thumbnail adListing">
-                            <a href="view.php?id=<?= $adIndex ?>"><img src="img/<?= $adContent->img; ?>" alt="<?= $adContent->title ?> Photo"></a>
+                            <a href="view.php?id=<?= $adContent->id; ?>"><img src="<?= $adContent->imagePath; ?>" alt="<?= $adContent->title ?> Photo"></a>
                             <div class="caption">
-                                <h3><a href="view.php?id=<?= $adIndex ?>"><?= $adContent->title; ?></a></h3>
-                                <p>Contact: <?= $adContent->username; ?></p>
-                                <p class="listDate"><em><?= $adContent->listDate; ?></em></p>
+                                <h3><a href="view.php?id=<?= $adContent->id; ?>"><?= $adContent->title; ?></a></h3>
+                                <p>Contact: <?= $adContent->contactName; ?></p>
+                                <p><em><?= $adContent->createdAt->format('l, F jS, Y');  ?></em></p>
                             </div>
                         </div>
                     </div>
