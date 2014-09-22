741bcacb620adef8fa687faa704d759b27ec22d6
haydogsup <joshua.r.mays@gmail.com>
2014-09-22 00:21:59 -0500

    added image property and method support for ad class

--- public/lister/classes/ad.class.php
+++ public/lister/classes/ad.class.php
@@ -1,24 +1,90 @@
 <?php
 
-// CLASS DEFINITION FOR INDIVIDUAL AD OBJECTS
 class Ad {
-    public $title = '';
-    public $body = '';
-    public $listDate = '';
-    public $username = '';
-    public $email = '';
-    public $img = '';
-    
-    public function __construct($title, $body, $listDate, $username, $email, $img) {
-        $this->title = $title;
-        $this->body = $body;
-        $this->listDate = $listDate;
-        $this->username = $username;
-        $this->email = $email;
-        $this->img = $img;
+    public $dbc;
+    public $id;
+
+    public $title        = '';
+    public $body         = '';
+    public $contactName  = '';
+    public $contactEmail = '';
+    public $createdAt    = '';
+    public $image_path   = '';
+
+    // CONSTRUCTOR FUNCTION FOR AD OBJECTS
+    public function __construct($dbc, $id = null) {
+        $this->dbc = $dbc;
+
+        // CHECKS IF THE AD ID ALREADY EXISTS (EDITING AN AD)
+        if (isset($id)) {
+            // ASSIGN ID FROM EXISTING AD TO OBJECT
+            $this->id = $id;
+
+            // STATEMENT TO READ IN AD PROPERTIES FROM THE DATABASE
+            $selectStmt = $this->dbc->prepare('SELECT * FROM items WHERE id = ?');
+            $selectStmt->execute([$this->id]);
+
+            // COPY ASSOCIATIVE ARRAY OF PROPERTIES FROM DATABASE RECORD
+            $row = $selectStmt->fetch(PDO::FETCH_ASSOC);
+
+            // ASSIGN OBJECT PROPERTIES FROM FETCH ASSOC ARRAY
+            $this->title        = $row['title'];
+            $this->body         = $row['body'];
+            $this->contactName  = $row['name'];
+            $this->contactEmail = $row['email'];
+            $this->createdAt    = new DateTime($row['created_at']);
+            $this->imagePath   = $row['image_path'];
+        }
     }
-    // METHOD TO RETURN AD OBJECT AS AN ARRAY
-    public function adArray() {
-        return [$this->title, $this->body, $this->listDate, $this->username, $this->email, $this->img];
+
+    // FUNCTION TO CHOOSE BETWEEN INSERTING AND UPDATING AN AD
+    // IF THE AD ID ALREADY EXISTS IN THE TABLE, UPDATE. IF NOT, INSERT.
+    public function save() {
+        if (isset($this->id)) {
+            $this->update();
+        } else {
+            $this->insert();
+        }
+    }
+
+    // FUNCTION TO INSERT NEW AD OBJECT INTO THE DATABASE
+    protected function insert() {
+        // CREATE A TIMESTAMP FOR AD ON INSERT
+        $this->createdAt = new DateTime();
+
+        // PREPARED INSERT SQL STATEMENT FOR NEW ADS
+        $insertSql = 'INSERT INTO items (title, body, name, email, created_at, image_path)
+                      VALUES (:title, :body, :name, :email, :created_at, :image_path)';
+
+        $insertStmt = $this->dbc->prepare($insertSql);
+
+        // BIND AD OBJECT PROPERTIES TO PREPARED STATEMENT VARIABLES
+        $insertStmt->bindValue(':title',      $this->title,                  PDO::PARAM_STR);
+        $insertStmt->bindValue(':body',       $this->body,                   PDO::PARAM_STR);
+        $insertStmt->bindValue(':name',       $this->contactName,            PDO::PARAM_STR);
+        $insertStmt->bindValue(':email',      $this->contactEmail,           PDO::PARAM_STR);
+        $insertStmt->bindValue(':created_at', $this->createdAt->format('c'), PDO::PARAM_STR);
+        $insertStmt->bindValue(':image_path', $this->imagePath,              PDO::PARAM_STR);
+        $insertStmt->execute();
+
+        // ASSIGN AD OBJECT FROM ID OF INSERTED RECORD FOR AD VIEW PAGE
+        $this->id = $this->dbc->lastInsertId();
+    }
+    
+    protected function update() {
+        // PREPARED UPDATE SQL STATEMENT FOR EDITING ADS
+        $updateSql = 'UPDATE items
+                      SET title = :title, body = :body, name = :name, email = :email, image_path = :image_path
+                      WHERE id = :id';
+
+        $updateStmt = $this->dbc->prepare($updateSql);
+        // BIND EDITED OBJECT PROPERTIES TO PREPARED STATEMENT VARIABLES
+        $updateStmt->bindValue(':title',      $this->title,        PDO::PARAM_STR);
+        $updateStmt->bindValue(':body',       $this->body,         PDO::PARAM_STR);
+        $updateStmt->bindValue(':name',       $this->contactName,  PDO::PARAM_STR);
+        $updateStmt->bindValue(':email',      $this->contactEmail, PDO::PARAM_STR);
+        $updateStmt->bindValue(':image_path', $this->imagePath,    PDO::PARAM_STR);
+        $updateStmt->bindValue(':id',         $this->id,           PDO::PARAM_INT);
+        $updateStmt->execute();
     }
 }
