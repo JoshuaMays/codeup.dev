9d06629404a5dcb10c330a7959b06ea9dcbb7e24
haydogsup <joshua.r.mays@gmail.com>
2014-09-29 13:14:09 -0500

    displaying all ads associated to a tag, using index as a reference

---
+++ public/lister/tag-show.php
@@ -0,0 +1,42 @@
+<?php 
+
+require_once('../../adlisterconnect.php');
+require_once('inc/TagManager.class.php');
+
+// CREATE AD MANAGER AND ADS ARRAY
+$TagManager = new TagManager($dbc);
+$ads = $TagManager->loadTaggedAds();
+
+require_once('header.php');
+
+?>
+
+<div id="pageWrap">
+    <div class="page-header">
+        <div class="col-sm-8 col-sm-offset-2">
+            <h1> <br><small>Search for an Ad, List an Ad. We don't care.</small></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <div class="row">
+                <? // LOOP THROUGH ADS TO DISPLAY ALL ADS IN DATABASE ?>
+                <? foreach($ads as $adIndex => $adContent): ?>
+                    <!-- ASSIGN CLEAR CLASS TO EVERY THIRD AD, SO ROWS OF THREE ADS LINE UP CORRECTLY -->
+                    <div class="col-sm-4 col-md-4 <?= $adIndex % 3 == 0 ? 'clear' : ''; ?>">
+                        <div class="thumbnail adListing">
+                            <a href="view.php?id=<?= $adContent->id; ?>"><img src="<?= "img/" . $adContent->imagePath; ?>" alt="<?= $adContent->title ?> Photo"></a>
+                            <div class="caption">
+                                <h3><a href="view.php?id=<?= $adContent->id; ?>"><?= $adContent->title; ?></a></h3>
+                                <p>Contact: <?= $adContent->contactName; ?></p>
+                                <p><em><?= $adContent->createdAt->format('l, F jS, Y');  ?></em></p>
+                            </div>
+                        </div>
+                    </div>
+                <? endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<? require_once('footer.php'); ?>
