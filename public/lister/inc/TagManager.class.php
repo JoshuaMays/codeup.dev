9133f7d0b5aacf6089248d01b6eb3e9bea58c091
haydogsup <joshua.r.mays@gmail.com>
2014-09-29 22:48:20 -0500

    finished functionality for loading a list of all tags in the tags table

--- public/lister/inc/TagManager.class.php
+++ public/lister/inc/TagManager.class.php
@@ -1,16 +1,20 @@
 <?php
 
 require_once('ad_manager.class.php');
+require_once('Tag.class.php');
 
 class TagManager extends AdManager {
 
-    protected $tag_id;
+    protected $tag_id = '';
 
     public function __construct($dbc) {
         $this->dbc = $dbc;
-        $this->tag_id = $_GET['tag'];
+        // CHECK IF USER IS TRYING TO VIEW ADS ASSOCIATE WITH A SPECIFIC TAG
+        if (isset($_GET['tag'])) {
+            $this->tag_id = $_GET['tag'];
+        }
     }
-    
+
     // METHOD FOR LOADING ALL ADS MATCHING THE TAG
     public function loadTaggedAds() {
         $adsSQL = 'SELECT a.id, a.title, a.name, a.created_at, a.image_path FROM ads a
@@ -31,5 +35,23 @@ class TagManager extends AdManager {
         }
 
         return $ads;
+    }
+
+    // METHOD FOR LOADING ALL AVAILABLE TAGS WHEN CREATING A NEW AD
+    public function loadTagCollection() {
+        $tagsSQL = 'SELECT id, tag FROM tags ORDER BY tag ASC';
+        $tagsStmt = $this->dbc->prepare($tagsSQL);
+        $tagsStmt->execute();
+        
+        $tagsList = [];
+        while($row = $tagsStmt->fetch(PDO::FETCH_ASSOC)) {
+
+            // CREATE A NEW TAG OBJECT FOR EACH ROW OF DATA IN tags TABLE
+            $tag = new Tag($this->dbc, $row['id']);
+
+            // PUSH TAGS ONTO AN ARRAY
+            $tagsList[] = $tag;
+        }
+        return $tagsList;
     }
 }
